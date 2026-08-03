b252f492215515a6ed5d2ff14511cd0c82bedaa6
Jawad Ali <149782239+jawadDev1@users.noreply.github.com>
2026-08-03 14:46:44 +0500

    feat: naruto langgraph

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 print("Hello world")
+"""
+Acceptance Criteria
+    - Understands the difference between a chain and a graph in LangGraph
+    - Can build a LangGraph workflow with at least 3 nodes and conditional edges
+    - Understands how to manage state across steps
+    - Can implement a human-in-the-loop interrupt in a graph
+    - Built a working agent that uses tools and loops until a goal is met
+Deliverable
+    - A GitHub repo with a LangGraph-based agent (e.g., research agent, task planner, or coding assistant) with a clear README
+
+
+    LangGraph Series (Full Playlist): https://www.youtube.com/watch?v=WPgG_PlOsYs&list=PLNIQLFWpQMRXmns-7UarmPIR6DN7bgEzZ
+"""
