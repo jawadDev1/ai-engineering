0f74d9316d4a0146eebd2b1df28ca151ac297f5a
Jawad Ali <149782239+jawadDev1@users.noreply.github.com>
2026-08-06 10:40:46 +0500

    feat: chatbot frontend

---
+++ agentic_ai_langgraph/chatbot/chatbot_backend.py
@@ -0,0 +1,37 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import  BaseMessage
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+load_dotenv()
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash", temperature=0.5 
+)
+
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+
+    res = llm.invoke(messages)
+
+    return {'messages': [res]}
+
+
+checkpointer = InMemorySaver()
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
